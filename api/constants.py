289ba57057b32ac683c0d077d24d1471c2b67f59
Stan van Rooy <me@stanvanrooy.com>
2022-05-21 16:35:27 +0200

    misc: remove auth_token

--- api/constants.py
+++ api/constants.py
@@ -1,5 +1,1 @@
-import os
-
 DATABASE = "./database.db"
-AUTH_TOKEN = os.environ.get("AUTH_TOKEN")
-
--- ui/src/constants.ts
+++ ui/src/constants.ts
@@ -1,3 +1,1 @@
-export const BASE_API_URL = 'http://127.0.0.1:5000/api' ?? process.env.BASE_API_URL;
-export const auth_token = process.env.AUTH_TOKEN ?? '';
-
+export const BASE_API_URL = 'http://127.0.0.1:5000/api';
