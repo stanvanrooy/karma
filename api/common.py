9f9fc432127f6bcbf6a4fee1da14c6a67b40d644
Stan van Rooy <me@stanvanrooy.com>
2022-05-21 18:49:46 +0200

    refactor: move table creation to separate functionc

--- api/common.py
+++ api/common.py
@@ -9,28 +9,35 @@ def get_db():
         return g.__db
 
     config = c.get_config()
-
     g.__db = sqlite3.connect(config.database.path)
-    try:
-        g.__db.execute("""CREATE TABLE alerts (
-            id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            status TEXT, 
-            labels TEXT, 
-            annotations TEXT, 
-            startsAt datetime, 
-            endsAt datetime,
-            generatorUrl TEXT
-        )""")
-    except Exception as e:
-        pass
-    try:
-        g.__db.execute("""CREATE TABLE notes (
-            id INTEGER PRIMARY KEY AUTOINCREMENT, 
-            alertId INTEGER, 
-            text TEXT, 
-            createdAt datetime DEFAULT (datetime('now','localtime'))
-        )""")
-    except:
-        pass
+    _ensure_tables()
     return g.__db
+
+
+def _ensure_tables():
+    _ensure_alert_table()
+    _ensure_note_table()
+
+
+def _ensure_alert_table():
+    db = get_db()
+    db.execute("""CREATE TABLE if not exists alerts (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        status TEXT, 
+        labels TEXT, 
+        annotations TEXT, 
+        startsAt datetime, 
+        endsAt datetime,
+        generatorUrl TEXT
+    )""")
+
+
+def _ensure_note_table():
+    db = get_db()
+    db.execute("""CREATE TABLE if not exists notes (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        alertId INTEGER, 
+        text TEXT, 
+        createdAt datetime DEFAULT (datetime('now','localtime'))
+    )""")
 
